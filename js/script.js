846b6b5b512bb680350dc74fc811ca73f78529b9
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2022-04-24 13:54:21 +0300

    update

--- js/script.js
+++ js/script.js
@@ -3,9 +3,33 @@
 class VideoFrame extends HTMLElement {
      constructor() {
         super();
-        const shadow =this.attachShadow({mode:'open'});
+        const shadow = this.attachShadow({mode:'open'});
+
+        //shadow.append('iframe');
+
+        let iframe = document.createElement('iframe');
+        iframe.setAttribute('src','https://www.youtube.com');
+        iframe.setAttribute('id','data-video-id');
+        iframe.setAttribute('data-height','height=50px');
+        iframe.setAttribute('data-width','width=500px');
+
+        shadow.appendChild(iframe);
      }
+
+
 }
 customElements.define('yt-video',VideoFrame);
 })();
 
+// class VideoFrame extends HTMLElement {
+//     constructor() {
+//         super();
+//         const shadow = this.attachShadow({mode:'open'});
+//
+//         shadow.append('body is good');
+//         const iframe = document.createElement("iframe");
+//     }
+//
+//
+// }
+// customElements.define('yt-video',VideoFrame);
